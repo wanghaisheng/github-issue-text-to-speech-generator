08e8275f6a32108c46aa416e8b01127325105662
Tigran <tigran.arsenyan.2018@gmail.com>
2022-02-06 15:46:29 +0400

    added text to speech functionality

--- Reddit/RedditService.py
+++ Reddit/RedditService.py
@@ -16,44 +16,8 @@ def startSession():
         password=utils.password,
     )
 
-    # auth = requests.auth.HTTPBasicAuth(utils.appID, utils.secretKey)
-    # data = {'grant_type': 'password',
-    #         'username': utils.username,
-    #         'password': utils.password}
-    #
-    # headers = {'User-Agent': 'MyBot/0.0.1'}
-    # res = requests.post('https://www.reddit.com/api/v1/access_token',
-    #                     auth=auth, data=data, headers=headers)
-    # TOKEN = res.json()['access_token']
-    # __headers = {**__headers, **{'Authorization': f"bearer {TOKEN}"}}
 
-def getPosts():
+def getHotSubmissions():
     subreddit = reddit.subreddit("askReddit")
 
-    for submission in subreddit.hot(limit=10):
-        print(submission.title)
-        print(submission.num_comments)
-        print(submission.comments)
-        print("-----------")
-        break
-
-
-    # requests.get('https://oauth.reddit.com/api/v1/me', headers=__headers)
-    # res = requests.get("https://oauth.reddit.com/r/askReddit/hot", headers=__headers)
-    # data = json.loads(res.json())
-    # jsonPosts = data["data"]["children"]
-    # posts = []
-    #
-    # for jsonPost in jsonPosts:
-    #     posts.append(utils.RedditPostTopic(id=jsonPost["id"],
-    #                                        url=jsonPost["url"],
-    #                                        subredditName=jsonPost["subreddit_name_prefixed"],
-    #                                        author=jsonPost["author"],
-    #                                        title=jsonPost["title"],
-    #                                        upvotes=jsonPost["ups"],
-    #                                        commentsCount=jsonPost["num_comments"]))
-    # print("post count {}".format(len(posts)))
-    # return  posts
-    # with open('filename.txt', 'w') as handle:
-    #     handle.write(json.dumps(res.json(), indent=4, sort_keys=True))
-    #     print(json.dumps(res.json(), indent=4, sort_keys=True))
+    return subreddit.hot(limit=10)
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-import random
-
 from Reddit import RedditService
 from selenium import webdriver
 from PIL import Image
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-
+import os
 import time
+import pyttsx3
+import shutil
+from random import randint
 
 driver = ""  ## the chrome web driver
+engine = pyttsx3.init()
+voices = ["karen", "samantha", "alex", "daniel"]
 
 
 def setDriver():
@@ -37,6 +40,54 @@ def startScrapping():
     screenshot()
 
 
-# startScrapping()
-RedditService.startSession()
-RedditService.getPosts()
+def mkdirIfExists(path):
+    if os.path.isdir(path):
+        print("path {} exists".format(path))
+        return
+    else:
+        os.mkdir(path)  # new directory
+
+
+def makeVideoFrom(submission):
+    folderPath = "./videos/new/askReddit_{}".format(submission.id)
+    if os.path.isdir(folderPath):
+        print("path {} exists, something is wrong".format(folderPath))
+        return
+    else:
+        os.mkdir(folderPath)  # new directory
+        print("creating new directory {}".format(folderPath))
+
+    makePostVideoAt(folderPath + "/post", submission)
+    makeCommentsVideosAt(folderPath + "/comments")
+
+
+def makePostVideoAt(path, submission):
+    mkdirIfExists(path)
+    #takePostScreenshotAt(path, submission)
+    makeVoiceAt(path, submission.title)
+    # clipTogether(screenshot, voice)
+    pass
+
+
+def makeVoiceAt(path, text):
+    engine.setProperty("rate", 210)
+    voice = voices[randint(0, len(voices) - 1)]
+    engine.setProperty("voice", "com.apple.speech.synthesis.voice.{}".format(voice))
+    engine.save_to_file(text, "speech.mp3")
+    engine.runAndWait()
+    engine.stop()
+    shutil.move("speech.mp3", path+"/speech.mp3")
+
+
+def makeCommentsVideosAt(path, submission):
+    pass
+
+
+if __name__ == '__main__':
+    # RedditService.startSession()
+    # submissions = RedditService.getHotSubmissions()
+    # for submission in submissions:
+    #     if submission.num_comments > 2000 and submission.score > 5000:
+    #         print("saving submisiion")
+    #         makeVideoFrom(submission)
+    makeVoiceAt("./videos/new/askReddit_{}".format("skzwrs"), "What is a socially unacceptable thing that you dont like or hate")
