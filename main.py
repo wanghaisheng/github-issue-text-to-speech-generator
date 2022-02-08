513e563644506ab676ece0aaadcce5574ebb56ce
Tigran <tigran.arsenyan.2018@gmail.com>
2022-02-08 20:12:08 +0400

    screenshoting comments

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import speechToVoiceService
 import screenshotService
 import os
 
+
 def mkdirIfExists(path):
     if os.path.isdir(path):
         print("path {} exists".format(path))
@@ -13,13 +14,7 @@ def mkdirIfExists(path):
 
 def makeVideoFrom(submission):
     folderPath = "./videos/new/askReddit_{}".format(submission.id)
-    if os.path.isdir(folderPath):
-        print("path {} exists, something is wrong".format(folderPath))
-        return
-    else:
-        os.mkdir(folderPath)  # new directory
-        print("creating new directory {}".format(folderPath))
-
+    mkdirIfExists(folderPath)
     makePostVideoAt(folderPath + "/post", submission)
     makeCommentsVideosAt(folderPath + "/comments", submission)
 
@@ -29,21 +24,28 @@ def makePostVideoAt(path, submission):
 
     screenshotService.screenshotPostAt(path, submission)
     speechToVoiceService.makeVoiceAt(path, submission.title)
-    # clipTogether(screenshot, voice)
+    clipTogether(path)
 
 
 def makeCommentsVideosAt(path, submission):
     mkdirIfExists(path)
-    pass
+    commentPerPostCount = 0
+    for comment in submission.comments:
+        if comment.score > 1000 and commentPerPostCount <= 10:
+            commentPerPostCount += 1
+            screenshotService.screenshotCommentAt(path, comment)
+            speechToVoiceService.makeVoiceAt(path, comment)
 
+
+def clipTogether(path):
+    pass
 
 if __name__ == '__main__':
-    pass
-    # RedditService.startSession()
-    # submissions = RedditService.getHotSubmissions()
-    # for submission in submissions:
-    #     if submission.num_comments > 2000 and submission.score > 5000:
-    #         print("saving submisiion")
-    #         makeVideoFrom(submission)
+    RedditService.startSession()
+    submissions = RedditService.getHotSubmissions()
+    for submission in submissions:
+        if submission.num_comments > 2000 and submission.score > 5000:
+            print("saving submisiion")
+            makeVideoFrom(submission)
     # speechToVoiceService.makeVoiceAt("./videos/new/askReddit_{}".format("skzwrs"), "What is a socially unacceptable thing that you dont like or hate")
-    screenshotService.screenshotPostAt("./videos/new/askReddit_{}".format("skzwrs"), "asd")
+    # screenshotService.screenshotPostAt("./videos/new/askReddit_{}".format("skzwrs"), "asd")
--- screenshotService.py
+++ screenshotService.py
@@ -2,28 +2,49 @@ from selenium import webdriver
 from PIL import Image
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
 _driverIsSetted = False
 
 def _setDriver():
-    global driver
-
+    options = Options()
+    options.add_argument("window-size=850,1000")
+    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
     url = "file:///Users/tigran/Desktop/redditPage.html"
     driver.get(url)
-    return driver
 
-
+    return  driver
 def screenshotPostAt(path, submission):
-    if not _driverIsSetted:
-        _setDriver()
-
-    global driver
+    driver = _setDriver()
 
     postElement = driver.find_element(By.CLASS_NAME, "_1oQyIsiPHYt6nx7VOmd1sz")
     titleElement = postElement.find_element(By.CLASS_NAME, "_eYtD2XCVieq6emjKBH3m")
-    driver.execute_script("arguments[0].innerText = 'asdasdasd'", titleElement)
+    upwoteDiv = postElement.find_element(By.CLASS_NAME, "_1rZYMD_4xY3gRcSS3p8ODO")
+    authorElement = postElement.find_element(By.CLASS_NAME, "_2tbHP6ZydRpjI44J3syuqC")
+
+    print(submission.author)
+    print(submission.score)
+    print(submission.title)
+    driver.execute_script('arguments[0].innerText = "u/{}"'.format(str(submission.author).replace('"', "'")), authorElement)
+    driver.execute_script('arguments[0].innerText = "{}"'.format(str(submission.score).replace('"', "'")), upwoteDiv)
+    driver.execute_script('arguments[0].innerText = "{}"'.format(str(submission.title).replace('"', "'")), titleElement)
 
     postElement.screenshot(path+"/screenshot.png")
     screenshot = Image.open(path+"/screenshot.png")
+    screenshot.show()
+
+def screenshotCommentAt(path, comment):
+    driver = _setDriver()
+
+    commentElement = driver.find_element(By.CLASS_NAME, "P8SGAKMtRxNwlmLz1zdJu")
+    authorElement  = commentElement.find_element(By.CLASS_NAME, "wM6scouPXXsFDSZmZPHRo")
+    upwoteElement = commentElement.find_element(By.CLASS_NAME, "_1rZYMD_4xY3gRcSS3p8ODO")
+    paragraphElement = commentElement.find_element(By.CLASS_NAME, "_1qeIAgB0cPwnLhDF9XSiJM")
+
+    driver.execute_script('arguments[0].innerText = "u/{}"'.format(str(comment.author.name).replace('"', "'")), authorElement)
+    driver.execute_script('arguments[0].innerText = "{}"'.format(str(comment.score).replace('"', "'")), upwoteElement)
+    driver.execute_script('arguments[0].innerText = "{}"'.format(str(comment.body).replace('"', "'")), paragraphElement)
+
+    commentElement.screenshot(path+"/comment.png")
+    screenshot = Image.open(path+"/comment.png")
     screenshot.show()
