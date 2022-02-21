5d5aa459bac71d623efa5f0d18114f0dcc7467ad
Tigran <tigran.arsenyan.2018@gmail.com>
2022-02-22 02:00:59 +0400

    added new ffmpeg concat demuxer

--- main.py
+++ main.py
@@ -47,16 +47,21 @@ def makeCommentsVideosAt(path, submission):
 
 
 def clipTogether(path):
+    mkdirIfExists(path + "/videos")
     bgVidoPath = "./videos/bgVideos/video1.mp4"
+    os.system("touch {}".format(path + "/videos/videos.txt"))
+    file = open(path + "/videos/videos.txt", 'w')
 
     bgClip = VideoFileClip(bgVidoPath)
     (w, h) = bgClip.size
     cropedBgClip = crop(bgClip, width=1080, height=1920, x_center=w/2, y_center=h/2)
     finalBgClip = concatenate_videoclips([cropedBgClip])
     # ffmpeg command for creating post video
-    os.system('ffmpeg -i {} -i {} -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -c:v libx264 -tune stillimage -c:a aac -b:a '
-              '192k -pix_fmt yuv420p {}'.format(path + "/post/screenshot.jpg", path + "/post/speech.mp3", path + "/post/out.mp4"))
+    os.system('ffmpeg -i {} -i {} -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -c:v libx264 -tune stillimage -b:v 1M -c:a aac -b:a '
+              '192k -pix_fmt yuv420p {}'.format(path + "/post/screenshot.jpg", path + "/post/speech.mp3", path + "/videos/out.mp4"))
+    file.write("file '/videos/out.mp4'\n".format(path))
 
+    # ffmpeg creating comments videos
     for fileName in os.listdir(path + "/comments"):
         if fileName.endswith(".jpg"):
             count = fileName.removeprefix("screenshot").removesuffix(".jpg")
@@ -64,15 +69,17 @@ def clipTogether(path):
             if os.path.isfile(path + "/comments/speech{}.mp3".format(count)):
                 print("is file")
                 os.system(
-                    'ffmpeg -i {} -i {} -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -c:v libx264 -tune stillimage -c:a aac -b:a '
+                    'ffmpeg -i {} -i {} -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -c:v libx264 -tune stillimage -b:v 1M -c:a aac -b:a '
                     '192k -pix_fmt yuv420p {}'.format(path + "/comments/screenshot{}.jpg".format(count), path + "/comments/speech{}.mp3".format(count),
-                                                      path + "/comments/out{}.mp4".format(count)))
+                                                      path + "/videos/out{}.mp4".format(count)))
+                file.write("file '/videos/out{}.mp4'\n".format(path, count) )
+    file.close()
 
-    print("asd clip together")
-    # final = CompositeVideoClip(finalBgClip)
-    # final.write_videofile("./videos/concat.mp4")
+    os.system("ffmpeg -f concat -i /videos/videos.txt -vf select=concatdec_select -af aselect=concatdec_select,aresample=async=1 /videos/finalOutput.mp4")
 
-if __name__ == '__main__':
+def main():
+    global screenshotService, speechToVoiceService
+
     RedditService.startSession()
     submissions = RedditService.getHotSubmissions()
     screenshotService = ScreenshotService()
@@ -81,3 +88,6 @@ if __name__ == '__main__':
     for submission in submissions:
         if submission.num_comments > 2000 and submission.score > 5000:
             makeVideoFrom(submission)
+
+if __name__ == '__main__':
+    main()
